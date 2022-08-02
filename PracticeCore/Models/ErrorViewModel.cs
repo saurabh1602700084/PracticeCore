7a50223bf71409b0785317b00373f4109ff7d232
saurabh1602700084 <patelsaurabh153@gmail.com>
2022-08-02 21:21:04 +0530

    Add project files.

---
+++ PracticeCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PracticeCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ PracticeCore/Models/Transcation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PracticeCore.Models
+{
+    public class Transcation
+    {
+        [Key]
+        public int TransactionId { get; set; }
+        [Column(TypeName="nvarchar(12)")]
+        public string AccountNumber { get; set; }
+        [Column(TypeName = "nvarchar(12)")]
+        public string Name { get; set; }
+        [Column(TypeName = "nvarchar(12)")]
+        public string BankName { get; set; }
+        [Column(TypeName = "nvarchar(12)")]
+        public string IfscCode { get; set; }
+        public int Account { get; set; }
+    }
+}
